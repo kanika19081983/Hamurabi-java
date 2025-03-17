0074ee3323af747bbab72461433f4678cb5d738c
Kanika <kanika@macbookpro.lan>
2025-03-16 22:18:03 -0400

    created xml file and started with Hammurabi

---
+++ src/main/java/Hamurabi.java
@@ -0,0 +1,26 @@
+package Hammurabi;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    public int people = 100;
+    public int bushels = 2800;
+    public int acresLand = 1000;
+    public int landValue = 19; // bushels/acre
+    private String Hammurabi;
+    public String name = Hammurabi;
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        public static void main(String\[\]args) {
+            new Hammurabi().playGame();
+        }
+
+        void playGame() {
+
+        }
+    }
+}
+
+
+
